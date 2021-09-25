cce2722a3e66e098fea3f82403624b432c9373b9
Louis Asanaka <25756888+LouisAsanaka@users.noreply.github.com>
2021-09-25 14:11:17 -0500

    fetch data from api instead of file

--- website/js/checklist.js
+++ website/js/checklist.js
@@ -1,47 +1,54 @@
 'use strict';
 
-let first = true
-for (const [dining_hall_name, food_items] of Object.entries(dining_data)) {
-    if (first) {
-        $('#dining-halls').append($(`
-            <option value="${dining_hall_name}" selected>${dining_hall_name}</option>
-        `))
-        first = false
-    } else {
-        $('#dining-halls').append($(`
-            <option value="${dining_hall_name}">${dining_hall_name}</option>
-        `))
-    }
-}
+let dining_data = null
 
-$("#dining-halls").on('change', function() {
-    let valueSelected = this.value
+fetch('get_hall_info')
+    .then(response => response.json())
+    .then(data => {
+        dining_data = data
+        let first = true
+        for (const [dining_hall_name, food_items] of Object.entries(data)) {
+            if (first) {
+                $('#dining-halls').append($(`
+                    <option value="${dining_hall_name}" selected>${dining_hall_name}</option>
+                `))
+                first = false
+            } else {
+                $('#dining-halls').append($(`
+                    <option value="${dining_hall_name}">${dining_hall_name}</option>
+                `))
+            }
+        }
 
-    $('#food-list').empty()
-    for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
-        $('#food-list').append($(`
-            <div class="row food-list-entry">
-                <div class="col-3"></div>
-                <div class="text-left col-3">
-                    <label for="hour">${food_name}</label>
-                </div>
-                <div class="col-3">
-                    <input type="number" id="${food_name}" min="0" max="10"/>
-                </div>
-            </div>
-        `))
-    }
-    // for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
-    //     $('#food-list').append($(`
-    //         <div class="food-list-entry">
-    //             <label for="hour">${food_name}</label>
-    //             <input type="number" id="hour" min="0" max="10" />
-    //             <br />
-    //         </div>
-    //     `))
-    // }
-});
-$("#dining-halls").trigger('change')
+        $("#dining-halls").on('change', function() {
+            let valueSelected = this.value
+        
+            $('#food-list').empty()
+            for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
+                $('#food-list').append($(`
+                    <div class="row food-list-entry">
+                        <div class="col-3"></div>
+                        <div class="text-left col-3">
+                            <label for="hour">${food_name}</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="number" id="${food_name}" min="0" max="10"/>
+                        </div>
+                    </div>
+                `))
+            }
+            // for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
+            //     $('#food-list').append($(`
+            //         <div class="food-list-entry">
+            //             <label for="hour">${food_name}</label>
+            //             <input type="number" id="hour" min="0" max="10" />
+            //             <br />
+            //         </div>
+            //     `))
+            // }
+        })
+        $("#dining-halls").trigger('change')
+    })
 
 $('#form-thingy').on('submit', function(e) {
     e.preventDefault()
