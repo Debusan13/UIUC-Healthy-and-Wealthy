f2f0f8d15e7e9f6c3697eee88b20d67d7318b207
21xiaofanli <21xiaofanli@gmail.com>
2021-09-25 02:19:12 -0500

    points n stuff

--- website/js/load_list.js
+++ website/js/load_list.js
@@ -23,13 +23,43 @@ $("#dining-halls").on('change', function() {
         $('#food-list').append($(`
             <div class="food-list-entry">
                 <label for="hour">${food_name}</label>
-                <input type="number" id="hour" min="0" max="10" />
+                <input type="number" id="${food_name}" min="0" max="10" />
                 <br />
             </div>
         `))
     }
 });
 $("#dining-halls").trigger('change')
+
+$('#form-thingy').on('submit', function(e) {
+    e.preventDefault()
+
+    let points = 0
+    $('.food-list-entry > input').each(function(index, element) {
+        let point = parseInt(element.value)
+        if (Number.isInteger(point)) {
+            points += point
+        }
+    })
+    const myname = $('name-field').val()
+    $('#point-label').html(myname + ', have ' + points + ' points available')
+    // window.localStorage.setItem('myname', myname)
+    // console.log(window.localStorage.getItem('myname'))
+    // window.localStorage.setItem('mypoints', points)
+})
+
+// $(function() {
+//     const myname = localStorage.getItem('myname')
+//     console.log(myname)
+//     const mypoints = localStorage.getItem('mypoints')
+//     if (myname !== null && mypoints !== null && myname !== undefined && mypoints !== undefined) {
+//         $('#point-label').html(myname + ', have ' + mypoints + ' points available')
+//     } else {
+//         $('#point-label').html('e')
+//     }
+// })
+
+
 
 // for (const [dining_hall_name, food_items] of Object.entries(dining_data)) {
 //     for (const [food_name, data] of Object.entries(food_items)) {
