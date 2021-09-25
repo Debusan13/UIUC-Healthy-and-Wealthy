7e3f2eb1a8ddb7ac07762bf8641f61547006e687
Ishaan Mathur <imathur3@illinois.edu>
2021-09-25 02:59:02 -0500

    👶

--- website/js/load_list.js
+++ website/js/load_list.js
@@ -21,13 +21,26 @@ $("#dining-halls").on('change', function() {
     $('#food-list').empty()
     for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
         $('#food-list').append($(`
-            <div class="food-list-entry">
-                <label for="hour">${food_name}</label>
-                <input type="number" id="hour" min="0" max="10" />
-                <br />
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="text-left col-3">
+                    <label for="hour">${food_name}</label>
+                </div>
+                <div class="col-3">
+                    <input type="number" id="number" min="0" max="10"/>
+                </div>
             </div>
         `))
     }
+    // for (const [food_name, data] of Object.entries(dining_data[valueSelected])) {
+    //     $('#food-list').append($(`
+    //         <div class="food-list-entry">
+    //             <label for="hour">${food_name}</label>
+    //             <input type="number" id="hour" min="0" max="10" />
+    //             <br />
+    //         </div>
+    //     `))
+    // }
 });
 $("#dining-halls").trigger('change')
 
